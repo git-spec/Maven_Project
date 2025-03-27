9feee014d9342aea7ad0449765a8bdcb35942b02
git-spec <inkyfischer@gmail.com>
2025-03-27 14:17:18 +0100

    test: Added method fibonacciNumber_shouldReturn0_whenFibonacciNumberIs0 to class FibonacciNumberTest.

--- src/test/java/org/example/FibonacciNumberTest.java
+++ src/test/java/org/example/FibonacciNumberTest.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FibonacciNumberTest {
+    @Test
+    void fibonacciNumber_shouldReturn0_whenFibonacciNumberIs0() {
+        // GIVEN
+        int n = 0;
+        int expexted = 0;
+        // WHEN
+        int actual = FibonacciNumber.getFibonacciNumber(n);
+        // THEN
+        assertEquals(expexted, actual);
+    }
 }
