ac48f769dc088f31a64e7f16343df6aec77b16d6
git-spec <inkyfischer@gmail.com>
2025-03-27 17:23:55 +0100

    feat: Adjusted method getFibonacciNumber in class FibonacciNumber for negative number 1.

--- src/main/java/org/example/FibonacciNumber.java
+++ src/main/java/org/example/FibonacciNumber.java
@@ -1,17 +1,17 @@
 package org.example;
 
 public class FibonacciNumber {
-    static int n = 31;
+    static int n = -3;
 
     public static int getFibonacciNumber(int n) {
         int prevNum = 0;
-        int currNum = 1;
+        int currNum = n > 0 ? 1 : -1;
         int nextNum = currNum + prevNum;
 
         if (n == 0) {
             return 0;
-        } else if (n == 1) {
-            return 1;
+        } else if (n == 1 || n == -1) {
+            return currNum;
         } else {
             for (int i = 1; i < n; i++) {
                 nextNum = currNum + prevNum;
