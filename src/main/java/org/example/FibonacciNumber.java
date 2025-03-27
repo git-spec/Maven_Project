378730077fda09ed7318f7a39efe9ce82557af31
git-spec <inkyfischer@gmail.com>
2025-03-27 16:27:20 +0100

    feat: Adjusted method getFibonacciNumber in class FibonacciNumber for number n.

--- src/main/java/org/example/FibonacciNumber.java
+++ src/main/java/org/example/FibonacciNumber.java
@@ -1,15 +1,29 @@
 package org.example;
 
 public class FibonacciNumber {
-    int n = 0;
+    static int n = 3;
 
     public static int getFibonacciNumber(int n) {
+        int prevNum = 0;
+        int currNum = 1;
+        int nextNum = currNum + prevNum;
+
         if (n == 0) {
             return 0;
         } else if (n == 1) {
             return 1;
         } else {
-            return n;
+
+            for (int i = 1; i < n; i++) {
+                nextNum = currNum + prevNum;
+                prevNum = currNum;
+                currNum = nextNum;
+            }
+            return nextNum;
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFibonacciNumber( n ));
     }
 }
