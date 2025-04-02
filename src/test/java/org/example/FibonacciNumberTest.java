a830112d944d517c33bca5bb685bd0879f0bac41
git-spec <inkyfischer@gmail.com>
2025-04-02 20:59:32 +0200

    refactor: Corrected variable name expected of FibonacciNumberTest.

--- src/test/java/org/example/FibonacciNumberTest.java
+++ src/test/java/org/example/FibonacciNumberTest.java
@@ -8,60 +8,60 @@ public class FibonacciNumberTest {
     void getFibonacciNumber_shouldReturn0_whenNumberIs0() {
         // GIVEN
         int n = 0;
-        int expexted = 0;
+        int expected = 0;
         // WHEN
         int actual = FibonacciNumber.getFibonacciNumber(n);
         // THEN
-        assertEquals(expexted, actual);
+        assertEquals(expected, actual);
     }
     @Test
     void getFibonacciNumber_shouldReturn1_whenNumberIs1() {
         // GIVEN
         int n = 1;
-        int expexted = 1;
+        int expected = 1;
         // WHEN
         int actual = FibonacciNumber.getFibonacciNumber(n);
         // THEN
-        assertEquals(expexted, actual);
+        assertEquals(expected, actual);
     }
     @Test
     void getFibonacciNumber_shouldReturn1_whenNumberIs2() {
         // GIVEN
         int n = 2;
-        int expexted = 1;
+        int expected = 1;
         // WHEN
         int actual = FibonacciNumber.getFibonacciNumber(n);
         // THEN
-        assertEquals(expexted, actual);
+        assertEquals(expected, actual);
     }
     @Test
     void getFibonacciNumber_shouldReturn13_whenNumberIs7() {
         // GIVEN
         int n = 7;
-        int expexted = 13;
+        int expected = 13;
         // WHEN
         int actual = FibonacciNumber.getFibonacciNumber(n);
         // THEN
-        assertEquals(expexted, actual);
+        assertEquals(expected, actual);
     }
     @Test
     void getFibonacciNumber_shouldReturnNeg1_whenNumberIsNeg1() {
         // GIVEN
         int n = -1;
-        int expexted = -1;
+        int expected = -1;
         // WHEN
         int actual = FibonacciNumber.getFibonacciNumber(n);
         // THEN
-        assertEquals(expexted, actual);
+        assertEquals(expected, actual);
     }
     @Test
     void getFibonacciNumber_shouldReturnNeg13_whenNumberIsNeg7() {
         // GIVEN
         int n = -7;
-        int expexted = -13;
+        int expected = -13;
         // WHEN
         int actual = FibonacciNumber.getFibonacciNumber(n);
         // THEN
-        assertEquals(expexted, actual);
+        assertEquals(expected, actual);
     }
 }
